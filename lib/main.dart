1c84e496ee4052ee78772f0ed92798c6f23710a3
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-07-28 22:04:32 +0530

    Create logic for game

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String displayExOh = '';
+  bool ohTurn = true; // O starts the game
+  List<String> displayExOh = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +31,16 @@ class _HomePageState extends State<HomePage> {
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
-              onTap: _tapped,
+              onTap: () {
+                _tapped(index);
+              },
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey[700]!),
                 ),
                 child: Center(
                   child: Text(
-                    displayExOh,
+                    displayExOh[index],
                     style: const TextStyle(color: Colors.white, fontSize: 40),
                   ),
                 ),
@@ -47,9 +50,84 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _tapped() {
+  void _tapped(int index) {
     setState(() {
-      displayExOh = 'O';
+      if (ohTurn) {
+        displayExOh[index] = 'O';
+      } else {
+        displayExOh[index] = 'X';
+      }
+
+      ohTurn = !ohTurn;
+      _checkWinner();
     });
+  }
+
+  void _checkWinner() {
+    //check 1st row
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != ' ') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    //check 2nd row
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != ' ') {
+      _showWinDialog(displayExOh[3]);
+    }
+
+    //check 3rd row
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != ' ') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    //check 1st column
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != ' ') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    //check 2nd column
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != ' ') {
+      _showWinDialog(displayExOh[1]);
+    }
+
+    //check 3rd column
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != ' ') {
+      _showWinDialog(displayExOh[2]);
+    }
+
+    //check diagonal (
+    if (displayExOh[6] == displayExOh[4] &&
+        displayExOh[6] == displayExOh[2] &&
+        displayExOh[6] != ' ') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    //check diagonal
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != ' ') {
+      _showWinDialog(displayExOh[0]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER IS: ' + winner),
+          );
+        });
   }
 }
